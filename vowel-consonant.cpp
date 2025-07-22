5e96e5a5847320147426b29629d5832e1231d177
varunp28 <varunparandkar@gmail.com>
2025-07-22 22:37:01 +0530

    Create vowel-consonant.cpp

---
+++ vowel-consonant.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout<<"Enter the charecter: ";
+    cin>> ch;
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    cout<<"The charecter is a vowel";
+    else
+    cout<<"The charecter is a consonant";
